9701c162c7f613643a87b27c8c1d0c16d7e559ef
Codakyello <olaoluwathompson8@gmail.com>
2024-09-14 18:29:00 +0100

    setting naming

--- controllers/settingController.js
+++ controllers/settingController.js
@@ -5,13 +5,13 @@ const { sendSuccessResponseData } = require("../utils/responseHelpers");
 module.exports.getSettings = catchAsync(async (req, res) => {
   const setting = await Setting.findOne({});
 
-  sendSuccessResponseData(res, "setting", setting);
+  sendSuccessResponseData(res, "settings", setting);
 });
 
 module.exports.createSettings = catchAsync(async (req, res) => {
   const newSetting = await Setting.create(req.body);
 
-  sendSuccessResponseData(res, "setting", newSetting);
+  sendSuccessResponseData(res, "settings", newSetting);
 });
 
 module.exports.updateSettings = catchAsync(async (req, res) => {
@@ -20,5 +20,5 @@ module.exports.updateSettings = catchAsync(async (req, res) => {
     runValidators: true,
   });
 
-  sendSuccessResponseData(res, "setting", setting);
+  sendSuccessResponseData(res, "settings", setting);
 });
