381c70197d9211ee762641ad0e3dcacfd66a0d13
Codakyello <olaoluwathompson8@gmail.com>
2024-10-10 23:59:36 +0100

    finished setting controller

--- controllers/settingController.js
+++ controllers/settingController.js
@@ -1,16 +1,22 @@
 const Setting = require("../models/settingModel");
+const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 const { sendSuccessResponseData } = require("../utils/responseHelpers");
 
 module.exports.getSettings = catchAsync(async (req, res) => {
   const setting = await Setting.findOne({});
+  if (!setting) {
+    throw new AppError("Settings not found", 404);
+  }
 
   sendSuccessResponseData(res, "settings", setting);
 });
 
 module.exports.createSettings = catchAsync(async (req, res) => {
-  const newSetting = await Setting.create(req.body);
+  const existingSetting = await Setting.findOne({});
+  if (existingSetting) throw new AppError("Settings already exist", 400);
 
+  const newSetting = await Setting.create(req.body);
   sendSuccessResponseData(res, "settings", newSetting);
 });
 
@@ -20,10 +26,14 @@ module.exports.updateSettings = catchAsync(async (req, res) => {
     runValidators: true,
   });
 
+  if (!setting) throw new AppError("Settings not found", 404);
+
   sendSuccessResponseData(res, "settings", setting);
 });
 
 module.exports.deleteSettings = catchAsync(async (req, res) => {
-  await Setting.findOneAndDelete({});
+  const setting = await Setting.findOneAndDelete({});
+  if (!setting) throw new AppError("Settings not found", 404);
+
   res.status(204).json();
 });
